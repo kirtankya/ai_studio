8895e5fff961f74f236c620afa52f37e35a46c30
kirtankya <kirtankyada610@gmail.com>
2026-04-09 22:04:03 +0530

    feat: configure custom headers for sitemap and robots.txt in next.config.mjs and add initial sitemap data

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Config options here
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/robots.txt',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
